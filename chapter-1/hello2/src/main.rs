5ed7ef837e8395c35c4e119569b3467b88e11073
Bassam Ismail <skippednote@skippednote.dev>
2021-04-17 21:54:13 +0530

    chapter 1

---
+++ chapter-1/hello2/src/main.rs
@@ -0,0 +1,15 @@
+fn greet_world() {
+    println!("Hello, world!");
+
+    let southern_germany = "Grüß Gott!";
+    let japan = "";
+    let regions = [southern_germany, japan];
+
+    for region in regions.iter() {
+        println!("{}", &region)
+    }
+}
+
+fn main() {
+    greet_world()
+}
---
+++ chapter-1/productivity/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a = 10;
+
+    // if a = 10 {
+    if a == 10 {
+        println!("a equals ten");
+    }
+}

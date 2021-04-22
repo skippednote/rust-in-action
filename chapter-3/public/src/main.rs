fea1220986b8b39fc234f4fdaee9bab0578c99ea
Bassam Ismail <skippednote@skippednote.dev>
2021-04-22 10:21:58 +0530

    chapter 3

---
+++ chapter-3/public/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, PartialEq)]
+pub enum FileState {
+    Open,
+    Closed,
+}
+#[derive(Debug)]
+pub struct File {
+    pub name: String,
+    data: Vec<u8>,
+    pub state: FileState,
+}
+
+impl File {
+    pub fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn main() {
+    let f7 = File::new("f7.txt");
+    println!("{:?}", f7);
+}
